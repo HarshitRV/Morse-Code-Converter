d73cd8d92c693f761e0d7a4a1ba54e44e52f93b6
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2021-09-30 13:15:38 +0530

    done basic ui setup

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 from tkinter import *
-from tkinter.ttk import *
 
-style = Style()
-
-style.configure('W.TButton', font =
-               ('calibri', 10, 'bold', 'underline'),
-                foreground = 'red')
 # Configs
-PINK = "#e2979c"
-RED = "#e7305b"
-GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
 
 # Buttons function
 
 def enter():
-    pass
+    normal_text = text_entry.get("1.0",'end-1c')
+    morse_text = morse_entry.get("1.0",'end-1c')
+    print(normal_text, morse_text)
+
+    text_entry.delete("0", END)
+    text_entry.insert("Herefiw iwefi oiwf")
 
 # UI SETUP
 
@@ -44,25 +39,7 @@ morse_entry.grid(row=3,column=0,columnspan=1)
 morse_entry.config(highlightcolor="black", highlightbackground="black")
 
 # Buttons
-convert_button = Button(window, text = 'Quit !',
-                style = 'W.TButton',
-             command = enter)
+convert_button = Button(window, text = 'Convert',command = enter, background="red")
 convert_button.grid(row=4, column=0)
 
 window.mainloop()
-
-# from morse import MorseCode
-
-# translate = MorseCode()
-
-# message = input("Enter a message: ")
-
-# encoded_message = translate.to_morse(message)
-
-# print(encoded_message)
-
-# decode = encoded_message
-
-# decoded_message = translate.to_english(decode)
-
-# print(decoded_message)
